3487076255da4290c91036be986a5c833fd93c7c
STUDENT Justin Astillero <Justin.Astillero@students.ittralee.ie>
2022-12-14 16:45:11 +0000

    Signed-off-by: STUDENT Justin Astillero <Justin.Astillero@students.ittralee.ie>

--- Test/Assets/carscript.cs
+++ Test/Assets/carscript.cs
@@ -7,7 +7,7 @@ using UnityEngine;
 public class carscript : MonoBehaviour
 {
     Vector3 velocity, acceleration;
-    float rotationSpeed = 180, thrust = 10;
+    float rotationSpeed = 100, thrust = 2;
 
     // Start is called before the first frame update
     void Start()
